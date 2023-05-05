1cc8c9b7bf736ed3691dcd643cf6f8a8b5bcbd4d
edukatiau <edukatiau@gmail.com>
2023-05-04 22:33:44 -0300

    Create VinteVinte.java

---
+++ IFSUL/condicionais/lista1/VinteVinte.java
@@ -0,0 +1,18 @@
+package IFSUL.condicionais.lista1;
+
+import java.util.Scanner;
+
+public class VinteVinte {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int idade = sc.nextInt();
+
+        if (idade > 20) {
+            System.out.println("ABEND");
+        } else if (idade < 20) {
+            System.out.println("TAG");
+        } else {
+            System.out.println("VIP");
+        }
+    }
+}
