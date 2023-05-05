7adc3621f6ba25fadf32411233cbd0024436472a
edukatiau <edukatiau@gmail.com>
2023-05-04 22:26:40 -0300

    Create OndeEstaoOsSinais1.java

---
+++ IFSUL/condicionais/lista1/OndeEstaoOsSinais1.java
@@ -0,0 +1,17 @@
+package IFSUL.condicionais.lista1;
+
+import java.util.Scanner;
+
+public class OndeEstaoOsSinais1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+
+        if (numero >= 0){
+            System.out.println("POS");
+        }
+        else {
+            System.out.println("NEG");
+        }
+    }
+}
