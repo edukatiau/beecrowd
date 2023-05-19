3997f7b0fd37961c9e3d58da7d5a279dc808ec6d
edukatiau <edukatiau@gmail.com>
2023-05-18 22:02:44 -0300

    Update TemMadeiraSuficiente.java

--- IFSUL/condicionais/lista7/TemMadeiraSuficiente.java
+++ IFSUL/condicionais/lista7/TemMadeiraSuficiente.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class TemMadeiraSuficiente {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int madeiras = sc.nextInt();
+
+        if (madeiras > 30){
+            System.out.println(">30");
+        } else if(madeiras < 30){
+            System.out.println("<30");
+        } else {
+            System.out.println("=30");
+        }
     }
 }
